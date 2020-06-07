f9d3c8faaaa49a054c0b94cfbe236229181998bf
Aquila Kalagbor <49174526+kalagboraquila@users.noreply.github.com>
2020-06-07 17:04:02 +0100

    Create quillz.php

---
+++ scripts/quillz.php
@@ -0,0 +1,10 @@
+<?php
+
+$Name = "Aquila Kalagbor";
+$ID = "HNG-05991";
+$Email = "quillz.ak@gmail.com";
+$Language = "PHP";
+
+echo ("Hello World, this is $Name with HNGi7 ID $ID using $Language for stage 2 task");
+
+?>
